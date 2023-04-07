cbeccee438b53ee85cbc68a940e13697cfbdc50e
gmichel <gmichel@helcim.com>
2023-04-07 14:09:31 -0600

    Testing code update

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 
 func helloHandler(_ context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// Get the name parameter from the request body
-	//name := request.QueryStringParameters["name"]
-
+	name := request.QueryStringParameters["name"]
+	fmt.Println(request)
 	// Create the response body
-	message := fmt.Sprintf("Hello, %s!", request)
+	message := fmt.Sprintf("Hello, %s!", name)
 
 	// Create the API Gateway response
 	response := events.APIGatewayProxyResponse{
